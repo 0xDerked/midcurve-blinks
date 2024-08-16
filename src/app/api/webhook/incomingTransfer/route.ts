a711acb0f73560e0f2ea3847d82a0d35cdbcc7f7
Derked <nazek8@gmail.com>
2024-08-16 16:16:15 -0400

    api key auth

--- src/app/api/webhook/incomingTransfer/route.ts
+++ src/app/api/webhook/incomingTransfer/route.ts
@@ -1,6 +1,11 @@
 import { NextResponse } from 'next/server'
 
 export const POST = async (req: Request) => {
+    const apiKey = req.headers.get('Authorization')?.split(' ')[1]
+    if (apiKey !== process.env.WEBHOOK_API_KEY) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
+
     console.log('Incoming transfer detected!')
     const body = await req.json()
     console.log(body)
