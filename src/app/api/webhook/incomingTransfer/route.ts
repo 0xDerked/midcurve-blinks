f22ec6efe139049d80c45ec693dee320791cbef5
Derked <nazek8@gmail.com>
2024-08-16 15:19:28 -0400

    return 200 on webhook

--- src/app/api/webhook/incomingTransfer/route.ts
+++ src/app/api/webhook/incomingTransfer/route.ts
@@ -1,5 +1,9 @@
+import { NextResponse } from 'next/server'
+
 export const POST = async (req: Request) => {
     console.log('Incoming transfer detected!')
     const body = await req.json()
     console.log(body)
+
+    return NextResponse.json({ message: 'Incoming transfer handled' }, { status: 200 })
 }
